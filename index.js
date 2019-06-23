defcd4d025c4bad43c8548c053575c0eeb9e05f0
SkyAx <alxndr.konkov@gmail.com>
2019-06-23 15:48:03 +0300

    Added folder structure project, path to statick files in server, changed view index.html files, added styles.

---
+++ index.js
@@ -0,0 +1,26 @@
+//Команда require запрашивает модуль из папки node_modules
+const express = require('express');
+const path = require('path');
+const app = express();
+
+//Настройка выдачи статических файлов
+app.use('/lib', express.static('node_modules/'));
+app.use('/css', express.static('css/'));
+app.use('/img', express.static('images/'));
+app.use('/js', express.static('js/'));
+
+
+//Настройка порта сервера
+app.set('port', process.env.PORT || 8080);
+
+//Обработка корневого маршрута
+app.get('/', function (req, res) {
+    //Отправляем файл страницы
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+//Просшуиваем порт на запросы
+app.listen(app.get('port'));
+
+
+
